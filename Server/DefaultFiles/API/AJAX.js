d5708abbb94ad0e0606ae7db83fd2aec5a5e4911
Andrew Rondeau <none@none>
2009-12-21 20:42:11 -0800

    Updated the AJAX system to not overwrite its function; this is in anticipation of server-side JavaScript using the same API, and thus the same generated code, as client-side Javascript

--- Server/DefaultFiles/API/AJAX.js
+++ Server/DefaultFiles/API/AJAX.js
@@ -1,15 +1,16 @@
 // based on code from http://www.w3schools.com/Ajax/ajax_browsers.asp
 
-function CreateHttpRequest()
-{
-   if (window.XMLHttpRequest)
-      // code for IE7+, Firefox, Chrome, Opera, Safari
-      return new XMLHttpRequest();
+if (typeof CreateHttpRequest != 'function')
+   CreateHttpRequest = function()
+   {
+      if (window.XMLHttpRequest)
+         // code for IE7+, Firefox, Chrome, Opera, Safari
+         return new XMLHttpRequest();
 
-   else if (window.ActiveXObject)
-     // code for IE6, IE5
-     return new ActiveXObject("Microsoft.XMLHTTP");
+      else if (window.ActiveXObject)
+        // code for IE6, IE5
+        return new ActiveXObject("Microsoft.XMLHTTP");
 
-   else
-      throw "Your browser does not support AJAX!";
-}
+      else
+         throw "Your browser does not support AJAX!";
+   }
