c8777a92bef8926b0c5b96583ca657698daa8d03
Andrew Rondeau <none@none>
2010-03-02 13:00:23 -0800

    Added a built-in factory for unsupported file types that just throws an exception
    --HG--
    branch :

---
+++ Server/ObjectCloud.Interfaces/Disk/UnsupportedFileHandlerFactory.cs
@@ -0,0 +1,44 @@
+﻿// Copyright 2009, 2010 Andrew Rondeau
+// This code is released under the LGPL license
+// For more information, see either DefaultFiles/Docs/license.wchtml or /Docs/license.wchtml
+
+using System;
+
+using ObjectCloud.Common;
+using ObjectCloud.Interfaces.Security;
+
+namespace ObjectCloud.Interfaces.Disk
+{
+    /// <summary>
+    /// Intended for use as a placeholder when a file type isn't supported
+    /// </summary>
+    public class UnsupportedFileHandlerFactory : IFileHandlerFactory
+    {
+        public void CreateFile(IFileId fileId)
+        {
+            throw new NotImplementedException("File type isn't supported");
+        }
+
+        public IFileHandler OpenFile(IFileId fileId)
+        {
+            throw new NotImplementedException("File type isn't supported");
+        }
+
+        public void CopyFile(IFileHandler sourceFileHandler, IFileId fileId, ID<IUserOrGroup, Guid>? ownerID)
+        {
+            throw new NotImplementedException("File type isn't supported");
+        }
+
+        public void RestoreFile(IFileId fileId, string pathToRestoreFrom, ID<IUserOrGroup, Guid> userId)
+        {
+            throw new NotImplementedException("File type isn't supported");
+        }
+
+        public FileHandlerFactoryLocator FileHandlerFactoryLocator
+        {
+            get { return _FileHandlerFactoryLocator; }
+            set { _FileHandlerFactoryLocator = value; }
+        }
+        private FileHandlerFactoryLocator _FileHandlerFactoryLocator;
+    }
+}
