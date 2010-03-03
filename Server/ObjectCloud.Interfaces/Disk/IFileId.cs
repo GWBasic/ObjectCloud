11ca1b7c582009c8dbc40ac7919945ac7a907852
Andrew Rondeau <none@none>
2010-03-03 13:58:47 -0800

    Fixed VS 2005 compilation errors
    --HG--
    branch :

--- Server/ObjectCloud.Interfaces/Disk/IFileId.cs
+++ Server/ObjectCloud.Interfaces/Disk/IFileId.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿// Copyright 2009, 2010 Andrew Rondeau
+// This code is released under the LGPL license
+// For more information, see either DefaultFiles/Docs/license.wchtml or /Docs/license.wchtml
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace ObjectCloud.Interfaces.Disk
