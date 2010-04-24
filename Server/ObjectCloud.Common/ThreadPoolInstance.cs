867f112a77d198aa3b54598fe4655e7adfaa2f17
Andrew Rondeau <none@none>
2010-04-24 15:37:34 -0700

    Added some missing copyright info
    --HG--
    branch :

--- Server/ObjectCloud.Common/ThreadPoolInstance.cs
+++ Server/ObjectCloud.Common/ThreadPoolInstance.cs
@@ -1,6 +1,11 @@
+// Copyright 2009, 2010 Andrew Rondeau
+// This code is released under the Simple Public License (SimPL) 2.0.  Some additional privelages are granted.
+// For more information, see either DefaultFiles/Docs/license.wchtml or /Docs/license.wchtml
+
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 
 namespace ObjectCloud.Common
