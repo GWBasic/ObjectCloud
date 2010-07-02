e44f015b7f6178a1053891b1258f14853cfa3603
Andrew Rondeau <none@none>
2010-07-02 16:47:14 -0700

    Added function to pretty-print dates within a known span class
    --HG--
    branch :

---
+++ Server/DefaultFiles/API/prettyPrintDates.js
@@ -0,0 +1,9 @@
+// Scripts: /API/jquery.js, /API/pretty.js
+
+$(document).ready(function() {
+   $('.date_ago').each(function(i, element)
+   {
+      var date = new Date(parseFloat(element.innerHTML));
+      element.innerHTML = prettyDate(date);
+   });
+});
