f4540fd8b9f1f627f9fb4b14d957a76eaf0893f9
Andrew Rondeau <none@none>
2010-07-07 13:56:57 -0700

    not checked in
    --HG--
    branch :

---
+++ Server/ObjectCloud.Interfaces/Disk/HasFileHandlerFactoryLocator.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2009, 2010 Andrew Rondeau
+// This code is released under the Simple Public License (SimPL) 2.0.  Some additional privelages are granted.
+// For more information, see either DefaultFiles/Docs/license.wchtml or /Docs/license.wchtml
+
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ObjectCloud.Interfaces.Disk
+{
+    public class HasFileHandlerFactoryLocator
+    {
+        public FileHandlerFactoryLocator FileHandlerFactoryLocator
+        {
+            get { return _FileHandlerFactoryLocator; }
+            set { _FileHandlerFactoryLocator = value; }
+        }
+        private FileHandlerFactoryLocator _FileHandlerFactoryLocator;
+    }
+}
