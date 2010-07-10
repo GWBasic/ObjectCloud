b456a07efb49bab3598986d3a68907b533ba599e
Andrew Rondeau <none@none>
2010-07-09 22:59:17 -0700

    Added some libraries that were missing
    --HG--
    branch :

---
+++ Server/DefaultFiles/API/shareDialog.js
@@ -0,0 +1,25 @@
+// Scripts: /API/jquery.js, /API/jquery-ui.js, /API/Url.js
+
+// jquery ui way of setting permissions on an object
+
+
+function shareDialog_show(filename)
+{
+   var html = '<div><iframe style="width: 100%; height: 100%" src="/AppFeeds/API/PermissionsFrame.wchtml?FileName=' + Url.encode(filename) + '"></iframe></div>';
+
+   var dialog = $(html).dialog(
+      {
+         modal:true,
+         position:top,
+         height: 500,
+         width: 800,
+         title: 'Share: ' + filename,
+         buttons:
+         {
+            "Ok": function()
+            {
+               $(this).dialog("close");
+            }
+         }
+      });
+}
