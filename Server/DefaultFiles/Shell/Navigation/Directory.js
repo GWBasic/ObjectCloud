18c3e4d3d82a89044108ba902bc9899586deae61
Andrew Rondeau <none@none>
2010-07-26 22:01:16 -0700

    Made progress with moving the directory view over to the template system

--- Server/DefaultFiles/Shell/Navigation/Directory.js
+++ Server/DefaultFiles/Shell/Navigation/Directory.js
@@ -118,166 +118,163 @@ function doDirectory(directory, filetypes, dir)
    });
 }
 
-
-
-
-
-   function displayFiles(directory, files)
-   {
-      var htmlBuilder = '<table><tr><th>File name</th><th>My Permission</th><th>Owner</th><th>Last Modified</th><th>Created</th></tr>';
+function displayFiles(directory, files)
+{
+   var htmlBuilder = '<table><tr><th>File name</th><th>My Permission</th><th>Owner</th><th>Last Modified</th><th>Created</th></tr>';
       
-      // Sort by name
-      files.sort(function(a, b)
-      {
-         var aS = a.Filename.toLowerCase();
-         var bS = b.Filename.toLowerCase();
+   // Sort by name
+   files.sort(function(a, b)
+   {
+      var aS = a.Filename.toLowerCase();
+      var bS = b.Filename.toLowerCase();
 
-         if (aS < bS)
-            return -1;
-         if (aS > bS)
-            return 1;
+      if (aS < bS)
+         return -1;
+      if (aS > bS)
+         return 1;
 
-         return 0;
-      });
+      return 0;
+   });
 
-      var menus = {};
+   var menus = {};
 
-      files.each(function(file, i)
-      {
-         // Filename and menu
-         htmlBuilder += '<tr><td><span style="font-size: 2em"><a id="' + i + '_link' + '" href="' + directory + file.Filename + '"';
+   for (var i = 0; i < files.length; i++)
+   {
+      var file = files[i];
 
-         if (file.TypeId != "directory")
-            htmlBuilder += ' target="_blank"';
+      // Filename and menu
+      htmlBuilder += '<tr><td><span style="font-size: 2em"><a id="' + i + '_link' + '" href="' + directory + '/' + file.Filename + '"';
 
-         htmlBuilder += '>' + file.Filename + '</a></span>';
+      if (file.TypeId != "directory")
+         htmlBuilder += ' target="_blank"';
 
-         // Metadata
-         htmlBuilder += "<td>" + file.Permission + "</td>";
-         htmlBuilder += "<td>" + file.Owner + "</td>";
-         htmlBuilder += "<td>" + new Date(file.LastModified) + "</td>";
-         htmlBuilder += "<td>" + new Date(file.Created) + "</td>";
-         htmlBuilder += "</tr>";
+      htmlBuilder += '>' + file.Filename + '</a></span></td>';
 
-         // Create the menu.
-         var menuItems = [];
+      // Metadata
+      htmlBuilder += "<td>" + file.Permission + "</td>";
+      htmlBuilder += "<td>" + file.Owner + "</td>";
+      htmlBuilder += "<td>" + new Date(file.LastModified) + "</td>";
+      htmlBuilder += "<td>" + new Date(file.Created) + "</td>";
+      htmlBuilder += "</tr>";
 
-         if (("Read" == file.Permission) || ("Write" == file.Permission) || ("Administer" == file.Permission))
-            menuItems.push(
-            {
-               name: 'View',
-               callback: function()
-               {
-                  window.open(directory + file.Filename);
-               }
-            });
+      // Create the menu.
+      var menuItems = [];
 
-         if (("Write" == file.Permission) || ("Administer" == file.Permission))
-            menuItems.push(
-            {
-               name: 'Edit',
-               callback: function()
-               {
-                  window.open(directory + file.Filename + '?Action=Edit');
-               }
-            });
-
-         if ("Administer" == file.Permission)
+      if (("Read" == file.Permission) || ("Write" == file.Permission) || ("Administer" == file.Permission))
+         menuItems.push(
          {
-            menuItems.push(
+            name: 'View',
+            callback: function()
             {
-               name: 'Permissions',
-               callback: function()
-               {
-                  window.open('/Shell/Security/Permissions.oc?FileName=' + directory + file.Filename);
-               }
-            });
+               window.open(directory + file.Filename);
+            }
+         });
 
-            menuItems.push(
+      if (("Write" == file.Permission) || ("Administer" == file.Permission))
+         menuItems.push(
+         {
+            name: 'Edit',
+            callback: function()
             {
-               separator: true
-            });
+               window.open(directory + file.Filename + '?Action=Edit');
+            }
+         });
 
-            menuItems.push(
-            {
-               name: 'Rename',
-               callback: function()
-               {
-                  var newFilename = prompt("Enter new file name", file.Filename);
-
-                  if (null != newFilename)
-                  {
-                     Dir.RenameFile(
-                        {
-                           OldFileName: file.Filename,
-                           NewFileName: newFilename
-                        },
-                        function() {});
-                  }
-               }
-            });
-
-            menuItems.push(
-            {
-               name: 'Defrag',
-               callback: function()
-               {
-                  new Ajax.Request(
-                     directory + file.Filename + '?Method=Vacuum',
-                     {
-                        method: 'post',
-                        onSuccess: function(transport)
-                        {
-                           alert("Vacuum successful");
-                        },
-                        onFailure: function(transport)
-                        {
-                           alert("Vacuum failed");
-                        }
-                     });
-               }
-            });
-
-            menuItems.push(
-            {
-               name: 'Get Server-Side Javascript Errors',
-               callback: function()
-               {
-                  window.open(directory + file.Filename + '?Method=GetServersideJavascriptErrors');
-               }
-            });
-
-            menuItems.push(
-            {
-               separator: true
-            });
-
-            menuItems.push(
-            {
-               name: 'Delete',
-               callback: function()
-               {
-                  if (confirm("Delete " + file.Filename))
-                  {
-                     Dir.DeleteFile(
-                        {
-                           FileName: file.Filename,
-                        },
-                        function() {});
-                  }
-               }
-            });
-         }
-
-         menus[i + '_link'] = menuItems;
-      });
-
-      htmlBuilder += '</table>';
-
-      $("FilesDiv").innerHTML = htmlBuilder;
-
-      for (var element in menus)
+      if ("Administer" == file.Permission)
       {
-         Proto.CreateMenu(menus[element], $(element));
+         menuItems.push(
+         {
+            name: 'Permissions',
+            callback: function()
+            {
+               window.open('/Shell/Security/Permissions.oc?FileName=' + directory + file.Filename);
+            }
+         });
+
+         menuItems.push(
+         {
+            separator: true
+         });
+
+         menuItems.push(
+         {
+            name: 'Rename',
+            callback: function()
+            {
+               var newFilename = prompt("Enter new file name", file.Filename);
+               if (null != newFilename)
+               {
+                  Dir.RenameFile(
+                     {
+                        OldFileName: file.Filename,
+                        NewFileName: newFilename
+                     },
+                     function() {});
+               }
+            }
+         });
+
+         menuItems.push(
+         {
+            name: 'Defrag',
+            callback: function()
+            {
+               new Ajax.Request(
+                  directory + file.Filename + '?Method=Vacuum',
+                  {
+                     method: 'post',
+                     onSuccess: function(transport)
+                     {
+                        alert("Vacuum successful");
+                     },
+                     onFailure: function(transport)
+                     {
+                        alert("Vacuum failed");
+                     }
+                  });
+            }
+         });
+
+         menuItems.push(
+         {
+            name: 'Get Server-Side Javascript Errors',
+            callback: function()
+            {
+               window.open(directory + file.Filename + '?Method=GetServersideJavascriptErrors');
+            }
+         });
+
+         menuItems.push(
+         {
+            separator: true
+         });
+
+         menuItems.push(
+         {
+            name: 'Delete',
+            callback: function()
+            {
+               if (confirm("Delete " + file.Filename))
+               {
+                  Dir.DeleteFile(
+                     {
+                        FileName: file.Filename,
+                     },
+                     function() {});
+               }
+            }
+         });
       }
+
+      menus[i + '_link'] = menuItems;
    }
+
+   htmlBuilder += '</table>';
+
+   $(".filesDiv").html(htmlBuilder);
+
+   /*for (var element in menus)
+   {
+      Proto.CreateMenu(menus[element], $(element));
+   }*/
+}
