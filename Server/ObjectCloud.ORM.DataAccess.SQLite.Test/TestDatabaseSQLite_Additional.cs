7a02cf185743e6cd7e723c8992d822a901780dc8
Andrew Rondeau <github@andrewrondeau.com>
2012-03-01 22:55:45 -0800

    Updated copyright date in .cs files

--- Server/ObjectCloud.ORM.DataAccess.SQLite.Test/TestDatabaseSQLite_Additional.cs
+++ Server/ObjectCloud.ORM.DataAccess.SQLite.Test/TestDatabaseSQLite_Additional.cs
@@ -1,4 +1,4 @@
-// Copyright 2009, 2010 Andrew Rondeau
+// Copyright 2009 - 2012 Andrew Rondeau
 // This code is released under the Simple Public License (SimPL) 2.0.  Some additional privelages are granted.
 // For more information, see either DefaultFiles/Docs/license.wchtml or /Docs/license.wchtml
 
