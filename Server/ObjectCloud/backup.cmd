e798c8a3e8ae9fee1cd6e3149b5cd57422d40ff3
Andrew Rondeau <github@andrewrondeau.com>
2012-03-01 23:01:15 -0800

    Updated a few more copyright dates

--- Server/ObjectCloud/backup.cmd
+++ Server/ObjectCloud/backup.cmd
@@ -1,4 +1,4 @@
-rem Copyright 2009, 2010 Andrew Rondeau
+rem Copyright 2009 - 2012 Andrew Rondeau
 rem This code is released under the Simple Public License (SimPL) 2.0.  Some additional privelages are granted.
 rem For more information, see either DefaultFiles/Docs/license.wchtml or /Docs/license.wchtml
 
